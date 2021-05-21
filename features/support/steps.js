33caf64439236162255fb714330a49f1d2dae585
Dominic Bett <dbett49@gmail.com>
2021-05-21 17:29:00 +0300

    sample file based results saving

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,5 +1,7 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const assert = require("assert").strict;
+const fs = require('fs')
+const path = require('path');
 
 Given("a variable set to {int}", function (number) {
   this.setTo(number);
@@ -11,4 +13,15 @@ When("I increment the variable by {int}", function (number) {
 
 Then("the variable should contain {int}", function (number) {
   assert.equal(this.variable, number);
+  const results = {
+    passed: 6,
+    failed: 2,
+    total: 8
+  }
+  writeResultsToFile(results)
 });
+
+function writeResultsToFile(results) {
+  const filePath = path.join(__dirname, results)
+  fs.writeFileSync(filePath, JSON.stringify(results))
+}
